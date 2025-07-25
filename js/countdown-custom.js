644fcb9431a41b34d6f727e6494eb6aadeca4f1b
mehrankhosravani <34156261+mehrankhosravani@users.noreply.github.com>
2025-07-25 21:24:29 +0300

    Add files via upload

---
+++ js/countdown-custom.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function() {
+        $(function () {
+            $('#defaultCountdown').countdown({until: new Date(2025,07,23,12)}); // year, month, date, hour
+        });
+});		
+
